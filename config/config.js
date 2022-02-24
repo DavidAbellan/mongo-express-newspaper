bd0d4b0eb3b4b9f06bae104db660b8e6f160fd03
DavidMotos <davidabellan@rocketmail.com>
2022-02-24 13:23:48 +0100

    1stDeploy

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
     development : {
         dialect : 'mysql',
         username: 'admin',
@@ -9,5 +9,15 @@ module.exports = {
     test : {
       dialect:'mysql',
       storage: ':memory;'
-    }
+   } }*/
+  
+  module.exports = {
+    development : {
+        dialect : 'mysql',
+        username: 'b95ec9467d693b',
+        password: '85d8fcd3',
+        database: 'heroku_61c351229f336dc',
+        host:'eu-cdbr-west-02.cleardb.net'
+    },
+    
   }
